fcf38a4e5f0f408408504269485782cec07def1e
Joel Storr <joel.storr@ewe.net>
2023-06-07 16:29:49 +0200

    SetUp

---
+++ lib/screens/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_shopware_6_api/helpers/api_helpers.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+final url = Uri.https('localhost', 'store-api/category');
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final myCategories = ShopwareApiHelper().getCategoreis();
+
+    return FutureBuilder(
+        future: myCategories,
+        builder: (ctx, snapshot) {
+          if (snapshot.data == null) {
+            return const Text('Waiting for Data');
+          }
+          return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (ctx, index) {
+                return ListTile(
+                  title: Text(snapshot.data![index]['name']),
+                );
+              });
+        });
+  }
+}
