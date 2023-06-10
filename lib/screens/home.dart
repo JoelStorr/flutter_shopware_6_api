91d7099b5599a61233572bbc47e457703cdd2ad9
Joel Storr <joel.storr@ewe.net>
2023-06-10 12:52:01 +0200

    Added Image Asset

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -16,22 +16,44 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final myCategories = ShopwareApiHelper().getMainNavigation();
 
-    return FutureBuilder(
-        future: myCategories,
-        builder: (ctx, snapshot) {
-          if (snapshot.data == null) {
-            return const Text('Waiting for Data');
-          }
-          return Expanded(
-            child: ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (ctx, index) {
-                  return ListTile(
-                    title:
-                        Text(snapshot.data![index]['breadcrumb'][1].toString()),
-                  );
-                }),
-          );
-        });
+    return Expanded(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.9,
+            height: 110,
+            clipBehavior: Clip.hardEdge,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              image: DecorationImage(
+                  image: AssetImage(
+                      'assets/images/taste_your_progress_banner_orange.png'),
+                  fit: BoxFit.cover),
+            ),
+          ),
+          FutureBuilder(
+              future: myCategories,
+              builder: (ctx, snapshot) {
+                if (snapshot.data == null) {
+                  return const Text('Waiting for Data');
+                }
+                return Expanded(
+                  child: ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (ctx, index) {
+                        return ListTile(
+                          title: Text(snapshot.data![index]['breadcrumb'][1]
+                              .toString()),
+                        );
+                      }),
+                );
+              }),
+        ],
+      ),
+    );
   }
 }
