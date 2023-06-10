b7b353aadd135e28809e18beb79b9bb205ce4292
Joel Storr <joel.storr@ewe.net>
2023-06-10 22:35:46 +0200

    Made Hompage Scrollable

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -15,42 +15,46 @@ final url = Uri.https('localhost', 'store-api/navigation/main-navigation');
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    final myCategories = ShopwareApiHelper().getMainNavigation();
+    final myCategories = ShopwareApiHelper().getMainNavigation()
 
     return Expanded(
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.9,
-            height: 110,
-            clipBehavior: Clip.hardEdge,
-            decoration: const BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                    color: Color.fromARGB(137, 70, 70, 70),
-                    offset: Offset(1.0, 2.0),
-                    blurRadius: 2.0)
-              ],
-              borderRadius: BorderRadius.all(Radius.circular(5.0)),
-              image: DecorationImage(
-                  image: AssetImage(
-                      'assets/images/taste_your_progress_banner_orange.png'),
-                  fit: BoxFit.cover),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            const SizedBox(
+              height: 50,
             ),
-          ),
-          FutureBuilder(
-              future: myCategories,
-              builder: (ctx, snapshot) {
-                if (snapshot.data == null) {
-                  return const Text('Waiting for Data');
-                }
-                return Expanded(
-                  child: GridView.builder(
-                      padding: EdgeInsets.all(20),
+            //TODO: Add Latest Product Cards
+            Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: 110,
+              clipBehavior: Clip.hardEdge,
+              decoration: const BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Color.fromARGB(137, 70, 70, 70),
+                      offset: Offset(1.0, 2.0),
+                      blurRadius: 2.0)
+                ],
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                image: DecorationImage(
+                    image: AssetImage(
+                        'assets/images/taste_your_progress_banner_orange.png'),
+                    fit: BoxFit.cover),
+              ),
+            ),
+            FutureBuilder(
+                future: myCategories,
+                builder: (ctx, snapshot) {
+                  if (snapshot.data == null) {
+                    return const Text('Waiting for Data');
+                  }
+                  return GridView.builder(
+                      shrinkWrap: true,
+                      physics: const ClampingScrollPhysics(),
+                      padding: const EdgeInsets.all(20),
                       gridDelegate:
                           const SliverGridDelegateWithFixedCrossAxisCount(
                         crossAxisCount: 2,
@@ -71,10 +75,10 @@ class _HomeScreenState extends State<HomeScreen> {
                           title: Text(snapshot.data![index]['breadcrumb'][1]
                               .toString()),
                         ); */
-                      }),
-                );
-              }),
-        ],
+                      });
+                }),
+          ],
+        ),
       ),
     );
   }
