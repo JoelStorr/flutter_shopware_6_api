91965d83b81ea4be4ecfdf034b55dc605e04885d
Joel Storr <joel.storr@ewe.net>
2023-06-13 14:16:35 +0200

    Added Material Page Route for internal Page navigation

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,9 +5,9 @@ import 'package:flutter_shopware_6_api/widgets/cards/category_cart.dart';
 import 'package:flutter_shopware_6_api/widgets/cards/new_product_card.dart';
 
 class HomeScreen extends StatefulWidget {
-  HomeScreen({super.key, required this.changeToCategoryView});
-  Function({required String categoryName, required String categoryID})
-      changeToCategoryView;
+  const HomeScreen({
+    super.key,
+  });
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -24,131 +24,150 @@ class _HomeScreenState extends State<HomeScreen> {
 
     final ScrollController _scrollController = ScrollController();
 
-    return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          const SizedBox(
-            height: 80,
-          ),
-          FutureBuilder(
-            future: latestProduct,
-            builder: (ctx, snapshot) {
-              if (snapshot.data == null) {
-                return const SizedBox(
-                  height: 0.5,
-                );
-              }
-              return SizedBox(
-                height: 230,
-                child: Scrollbar(
-                  thumbVisibility: true,
-                  controller: _scrollController,
-                  child: ListView.separated(
-                    controller: _scrollController,
-                    scrollDirection: Axis.horizontal,
-                    itemCount: snapshot.data!.length,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      if (snapshot.data![index].containsKey('properties') &&
-                          snapshot.data![index]['properties'].length > 0) {
-                        return NewProductCard(
-                          id: snapshot.data![index]['id'],
-                          name: snapshot.data![index]['translated']['name'],
-                          imageURL: snapshot.data![index]['cover']['media']
-                              ['url'],
-                          buttonColor: snapshot.data![index]['properties'][0]
-                              ['colorHexCode'],
-                        );
-                      } else {
-                        return NewProductCard(
-                          id: snapshot.data![index]['id'],
-                          name: snapshot.data![index]['translated']['name'],
-                          imageURL: snapshot.data![index]['cover']['media']
-                              ['url'],
-                        );
-                      }
-                    },
-                    separatorBuilder: (context, index) => const SizedBox(
-                      width: 10,
+    return Navigator(
+      onGenerateRoute: (RouteSettings settings) {
+        return MaterialPageRoute(
+          settings: settings,
+          builder: (BuildContext context) {
+            return Scaffold(
+              //TODO: Add App bar
+              body: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    const SizedBox(
+                      height: 80,
                     ),
-                  ),
-                ),
-              );
-            },
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          /* FutureBuilder(builder: (){}), */
-
-          Container(
-            width: MediaQuery.of(context).size.width * 0.9,
-            height: 110,
-            clipBehavior: Clip.hardEdge,
-            decoration: const BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                    color: Color.fromARGB(137, 70, 70, 70),
-                    offset: Offset(1.0, 2.0),
-                    blurRadius: 2.0)
-              ],
-              borderRadius: BorderRadius.all(Radius.circular(5.0)),
-              image: DecorationImage(
-                  image: AssetImage(
-                      'assets/images/taste_your_progress_banner_orange.png'),
-                  fit: BoxFit.cover),
-            ),
-          ),
-          FutureBuilder(
-              future: mainNavigation,
-              builder: (ctx, snapshot) {
-                if (snapshot.data == null) {
-                  return const Center(
-                    heightFactor: 50.0,
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                return GridView.builder(
-                    shrinkWrap: true,
-                    physics: const ClampingScrollPhysics(),
-                    padding: const EdgeInsets.all(20),
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 3 / 2,
-                      crossAxisSpacing: 15,
-                      mainAxisSpacing: 15,
-                    ),
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (ctx, index) {
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (BuildContext context) {
-                                return CategoryScreen();
-                              },
-                            ),
+                    FutureBuilder(
+                      future: latestProduct,
+                      builder: (ctx, snapshot) {
+                        if (snapshot.data == null) {
+                          return const SizedBox(
+                            height: 0.5,
                           );
-                        },
-                        child: CategoryCart(
-                          title:
-                              snapshot.data![index]['breadcrumb'][1].toString(),
-                          imageLink: snapshot.data![index]['media']['url'],
-                          categoryId: snapshot.data![index]['id'],
-                        ),
-                      );
+                        }
+                        return SizedBox(
+                          height: 230,
+                          child: Scrollbar(
+                            thumbVisibility: true,
+                            controller: _scrollController,
+                            child: ListView.separated(
+                              controller: _scrollController,
+                              scrollDirection: Axis.horizontal,
+                              itemCount: snapshot.data!.length,
+                              shrinkWrap: true,
+                              itemBuilder: (context, index) {
+                                if (snapshot.data![index]
+                                        .containsKey('properties') &&
+                                    snapshot.data![index]['properties'].length >
+                                        0) {
+                                  return NewProductCard(
+                                    id: snapshot.data![index]['id'],
+                                    name: snapshot.data![index]['translated']
+                                        ['name'],
+                                    imageURL: snapshot.data![index]['cover']
+                                        ['media']['url'],
+                                    buttonColor: snapshot.data![index]
+                                        ['properties'][0]['colorHexCode'],
+                                  );
+                                } else {
+                                  return NewProductCard(
+                                    id: snapshot.data![index]['id'],
+                                    name: snapshot.data![index]['translated']
+                                        ['name'],
+                                    imageURL: snapshot.data![index]['cover']
+                                        ['media']['url'],
+                                  );
+                                }
+                              },
+                              separatorBuilder: (context, index) =>
+                                  const SizedBox(
+                                width: 10,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    /* FutureBuilder(builder: (){}), */
 
-                      /* ListTile(
-                        title: Text(snapshot.data![index]['breadcrumb'][1]
-                            .toString()),
-                      ); */
-                    });
-              }),
-        ],
-      ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      height: 110,
+                      clipBehavior: Clip.hardEdge,
+                      decoration: const BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              color: Color.fromARGB(137, 70, 70, 70),
+                              offset: Offset(1.0, 2.0),
+                              blurRadius: 2.0)
+                        ],
+                        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                        image: DecorationImage(
+                            image: AssetImage(
+                                'assets/images/taste_your_progress_banner_orange.png'),
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                    FutureBuilder(
+                        future: mainNavigation,
+                        builder: (ctx, snapshot) {
+                          if (snapshot.data == null) {
+                            return const Center(
+                              heightFactor: 50.0,
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+                          return GridView.builder(
+                              shrinkWrap: true,
+                              physics: const ClampingScrollPhysics(),
+                              padding: const EdgeInsets.all(20),
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                childAspectRatio: 3 / 2,
+                                crossAxisSpacing: 15,
+                                mainAxisSpacing: 15,
+                              ),
+                              itemCount: snapshot.data!.length,
+                              itemBuilder: (ctx, index) {
+                                return GestureDetector(
+                                  onTap: () {
+                                    Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                        builder: (BuildContext context) {
+                                          return CategoryScreen();
+                                        },
+                                      ),
+                                    );
+                                  },
+                                  child: CategoryCart(
+                                    title: snapshot.data![index]['breadcrumb']
+                                            [1]
+                                        .toString(),
+                                    imageLink: snapshot.data![index]['media']
+                                        ['url'],
+                                    categoryId: snapshot.data![index]['id'],
+                                  ),
+                                );
+
+                                /* ListTile(
+                          title: Text(snapshot.data![index]['breadcrumb'][1]
+                              .toString()),
+                        ); */
+                              });
+                        }),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
