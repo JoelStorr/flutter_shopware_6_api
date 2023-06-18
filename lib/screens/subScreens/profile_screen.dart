4f7b38874f2eafe5c38caa01b918e9327d0d30c5
Joel Storr <joel.storr@ewe.net>
2023-06-18 11:38:48 +0200

    Start added the Profile Screen

--- lib/screens/subScreens/profile_screen.dart
+++ lib/screens/subScreens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shopware_6_api/widgets/buttons/main_pill_button.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -10,6 +11,55 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Profile Screen');
+    return Scaffold(
+      appBar: AppBar(
+        scrolledUnderElevation: 0.0,
+        title: Image.asset(
+          'assets/images/logo/Logo.png',
+          width: 200,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search,
+            ),
+            padding: const EdgeInsets.only(right: 30.0),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                const SizedBox(
+                  height: 100,
+                ),
+                FutureBuilder(
+                  //TODO: Make Profile Request
+                    future: ,
+                    builder: (ctx, snapshot) {}),
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              MainPillButton(
+                activeElement: 'right',
+                textLeft: 'More',
+                textRight: 'Profile',
+                navigateTo: const ProfileScreen(),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
